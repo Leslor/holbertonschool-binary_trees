756b9a22cb7d5853ab3cd0732218c7125154188c
LesLor <leslie.paz.am@gmail.com>
2022-07-22 02:16:08 +0000

    Fix: Task-17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,11 +8,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent = NULL;
 
 	if (!node || !parent)
 		return (NULL);
-	/*The hight of left and right*/
+	parent = node->parent;
 	if (parent->left && parent->right)
 	{
 		if (parent->left == node)
